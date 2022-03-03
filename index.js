e34dc980835b0941c0a92270df46ea8ff1fab68b
Ryan Wolton <ryanwolton@Ryans-MacBook-Pro.local>
2022-03-03 10:11:24 +0000

    differentiate values and operators

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const assert = require("assert")
 
-const Node = (operator, value, left, right) => {
-  const result = function () {
+const OperatorNode = (operator, left, right) => {
+  const result = function() {
     switch (this.operator) {
       case "+":
         return left.result() + right.result();
@@ -11,42 +11,44 @@ const Node = (operator, value, left, right) => {
         return left.result() * right.result();
       case "÷":
         return left.result() / right.result();
-      default:
-        return value;
     }
   };
 
-  
-  const toString = function () {
-    return this.operator ? `(${left.toString()} ${this.operator} ${right.toString()})` : value.toString();
+  const toString = function() {
+    return `(${left.toString()} ${operator} ${right.toString()})`;
   };
 
   return {
     operator,
-    value,
-    left,
-    right,
     result,
     toString
-  };
+  }
 };
 
-const tree = Node(
-  "÷",
-  null,
-  Node(
-    "+",
-    null,
-    Node("", 7, null, null),
-    Node(
-      "x",
-      null,
-      Node("-", null, Node("", 3, null, null), Node("", 2, null, null)),
-      Node("", 5, null, null)
-    )
-  ),
-  Node("", 6, null, null)
-);
+const ValueNode = (value) => {
+  const result = () => {
+    return value;
+  };
+
+  const toString = () => {
+    return value.toString();
+  };
+
+  return {
+    result,
+    toString
+  }
+};
+
+const Value2 = ValueNode(2)
+const Value3 = ValueNode(3)
+const Value7 = ValueNode(7)
+const Value6 = ValueNode(6)
+const Value5 = ValueNode(5)
+const SubtractNode = OperatorNode("-", Value3, Value2)
+const MultiplyNode = OperatorNode("x", SubtractNode, Value5)
+const AddNode = OperatorNode("+", Value7, MultiplyNode)
+const tree = OperatorNode("÷", AddNode, Value6)
 
 assert.strictEqual("((7 + ((3 - 2) x 5)) ÷ 6)", tree.toString());
 assert.strictEqual(2, tree.result());
