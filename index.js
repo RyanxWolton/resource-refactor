3ac9af48549d253357a88ff32ecf9a8c51f3d176
Ryan Wolton <ryanwolton@Ryans-MacBook-Pro.local>
2022-03-03 17:50:43 +0000

    Make math factory

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
 const assert = require("assert")
 
-const OperatorNode = (operator, left, right) => {
-  const result = function() {
-    switch (this.operator) {
-      case "+":
-        return left.result() + right.result();
-      case "-":
-        return left.result() - right.result();
-      case "x":
-        return left.result() * right.result();
-      case "÷":
-        return left.result() / right.result();
+const Math = {
+  OperatorNode: (operator, left, right) => ({
+    result() {
+      switch (operator) {
+        case "+":
+          return left.result() + right.result();
+        case "-":
+          return left.result() - right.result();
+        case "x":
+          return left.result() * right.result();
+        case "÷":
+          return left.result() / right.result();
+      }
+    },
+    toString() { 
+      return `(${left.toString()} ${operator} ${right.toString()})` 
     }
-  };
+  }),
+  ValueNode: (value) => ({
+    result() { return value },
+    toString() { return value.toString() }
+  })
+}
 
-  const toString = function() {
-    return `(${left.toString()} ${operator} ${right.toString()})`;
-  };
-
-  return {
-    operator,
-    result,
-    toString
-  }
-};
-
-const ValueNode = (value) => {
-  const result = () => {
-    return value;
-  };
-
-  const toString = () => {
-    return value.toString();
-  };
-
-  return {
-    result,
-    toString
-  }
-};
-
-const Value2 = ValueNode(2)
-const Value3 = ValueNode(3)
-const Value7 = ValueNode(7)
-const Value6 = ValueNode(6)
-const Value5 = ValueNode(5)
-const SubtractNode = OperatorNode("-", Value3, Value2)
-const MultiplyNode = OperatorNode("x", SubtractNode, Value5)
-const AddNode = OperatorNode("+", Value7, MultiplyNode)
-const tree = OperatorNode("÷", AddNode, Value6)
+const Two = Math.ValueNode(2)
+const Three = Math.ValueNode(3)
+const Five = Math.ValueNode(5)
+const Six = Math.ValueNode(6)
+const Seven = Math.ValueNode(7)
+const SubtractNode = Math.OperatorNode("-", Three, Two)
+const MultiplyNode = Math.OperatorNode("x", SubtractNode, Five)
+const AddNode = Math.OperatorNode("+", Seven, MultiplyNode)
+const tree = Math.OperatorNode("÷", AddNode, Six)
 
 assert.strictEqual("((7 + ((3 - 2) x 5)) ÷ 6)", tree.toString());
 assert.strictEqual(2, tree.result());
